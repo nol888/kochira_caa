12b9d4d328e609d9fcd81914993cbd90a6abc890
Nolan Lum <nolan@nolm.name>
2016-07-15 12:25:41 -0700

    Add yelp generator.

---
+++ kochira_caa/yelp.py
@@ -0,0 +1,126 @@
+"""
+Yelp generator plugin.
+
+Fake service that extends the generator service to provide
+typical Yelp imports.
+"""
+from functools import partial
+
+from kochira.service import Service
+from kochira.services.textproc.generators import PickFrom, WrapWith, RandomInt, run_generator, bind_generator
+
+service = Service(__name__, __doc__)
+
+
+@service.shutdown
+def reload_generators(ctx):
+    ctx.bot.unload_service(".textproc.generators")
+    ctx.bot.load_service(".textproc.generators", reload=True)
+
+
+yelp = partial(run_generator,
+               WrapWith(1,
+                        "from {}",
+                        PickFrom(1, [
+                            "core",
+                            "cmds",
+                            "util",
+                        ])
+               ),
+               PickFrom(RandomInt(2, 5), [
+                   WrapWith(1,
+                            PickFrom(1, [
+                                ".{}",
+                                PickFrom(1, [
+                                    ".ad_{}",
+                                    ".business_{}",
+                                    ".biz_{}",
+                                    ".photo_{}",
+                                    ".user_{}",
+                                ]),
+                            ]),
+                            PickFrom(1, [
+                                "common",
+                                "component",
+                                "lib",
+                                "models",
+                                "presentation",
+                                "util",
+                            ])
+                   ),
+               ]),
+               WrapWith(1,
+                        " import {}",
+                        PickFrom(1, [
+                            [
+                                PickFrom(1, [
+                                    "",
+                                    "_",
+                                ]),
+                                PickFrom(1, [
+                                    "maybe_",
+                                    "cached_",
+                                ]),
+                                PickFrom(1, [
+                                    "get_",
+                                    "check_",
+                                    "create_",
+                                    "log_",
+                                    "is_",
+                                    "format_",
+                                    "render_",
+                                ]),
+                                PickFrom(1, [
+                                    "",
+                                    "active_",
+                                    "next_",
+                                ]),
+                                PickFrom(1, [
+                                    "user",
+                                    "business",
+                                    "review",
+                                    "advertiser",
+                                ]),
+                                PickFrom(RandomInt(0, 2), [
+                                    "_config",
+                                    "_presenter",
+                                    "_params",
+                                    "_event",
+                                ]),
+                                PickFrom(1, [
+                                    "",
+                                    PickFrom(1, [
+                                        "_if_enabled",
+                                        "_if_active",
+                                    ]),
+                                ]),
+                            ],
+                            [
+                                PickFrom(RandomInt(1, 2), [
+                                    "Review",
+                                    "Biz",
+                                    "Business",
+                                    "Signup",
+                                    "User",
+                                ]),
+                                PickFrom(2, [
+                                    "Repository",
+                                    "Stub",
+                                    "Checkout",
+                                ]),
+                                PickFrom(1, [
+                                    "Params",
+                                    "Type",
+                                    "",
+                                ]),
+                            ],
+                    ])
+               )
+               )
+
+bind_generator("yelp", yelp,
+"""
+Yelp programmer simulator.
+
+Generates a typical Yelp import.
+""")
