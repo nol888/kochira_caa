34ec2e6abb5dd1e1ea2f588b4560db8f9a9250c8
Nolan Lum <nolan@nolm.name>
2016-07-15 12:25:32 -0700

    Add :a-drei:

--- kochira_caa/natto.py
+++ kochira_caa/natto.py
@@ -12,10 +12,12 @@ service = Service(__name__, __doc__)
 @service.setup
 def load_brain(ctx):
     ctx.storage.brain = Brain("natto.db", check_same_thread=False)
+    ctx.storage.adrei = Brain("a-drei.db", check_same_thread=False)
 
 @service.shutdown
 def unload_brain(ctx):
     ctx.storage.brain.graph.close()
+    ctx.storage.adrei.graph.close()
 
 @service.command(r":natto:", mention=False)
 @background
@@ -26,3 +28,8 @@ def generate_natto(ctx):
         ctx.message('<nattofriends> ' + msg[-1])
     else:
         ctx.message('<nattofriends> ' + msg)
+
+@service.command(r":a-drei:", mention=False)
+@background
+def generate_adrei(ctx):
+    ctx.message('\x02[@cute_hospital]\x02 ' + ctx.storage.adrei.reply(''))
