64470fc2bdb5063e27c9006314440510937aed43
nattofriends <nattofriends@users.noreply.github.com>
2017-10-02 20:21:59 -0700

    Update imouto.py

--- kochira_caa/imouto.py
+++ kochira_caa/imouto.py
@@ -11,6 +11,25 @@ stemmer = SnowballStemmer('english')
 
 service = Service(__name__, __doc__)
 
+def case_corrected_imouto(word, imouto):
+    if all(c.isupper() for c in word):
+        return imouto.upper()
+
+    if word.capitalize() == word:
+        return imouto.capitalize()
+
+    return imouto
+
+def imouto_factory(imouto):
+    def stemmed_imouto(match):
+        word = match.group(0)
+        word_stem = stemmer.stem(word)
+        if word != word_stem and word.startswith(word_stem):
+            return case_corrected_imouto(word, imouto) + word[len(word_stem):]
+        else:
+            return case_corrected_imouto(word, imouto)
+    return stemmed_imouto
+
 @service.command("!imouto(?: (?P<text>.+))?")
 def imouto(ctx, text=None):
     """
@@ -24,22 +43,22 @@ def imouto(ctx, text=None):
 
         _, text = ctx.client.backlogs[ctx.target][1]
 
-    def case_corrected_imouto(word):
-        if all(c.isupper() for c in word):
-            return 'IMOUTO'
+    text = re.sub("\w\w\w\w+", imouto_factory('imouto'), text)
+    ctx.message(text)
 
-        if word.capitalize() == word:
-            return 'Imouto'
 
-        return 'imouto'
+@service.command("!kimchi(?: (?P<text>.+))?")
+def kimchi(ctx, text=None):
+    """
+    Korean Imouto.
 
-    def stemmed_imouto(match):
-        word = match.group(0)
-        word_stem = stemmer.stem(word)
-        if word != word_stem and word.startswith(word_stem):
-            return case_corrected_imouto(word) + word[len(word_stem):]
-        else:
-            return case_corrected_imouto(word)
+    Imouto-ify some Korean.
+    """
+    if text is None:
+        if len(ctx.client.backlogs[ctx.target]) == 1:
+            return
 
-    text = re.sub("\w\w\w\w+", stemmed_imouto, text)
+        _, text = ctx.client.backlogs[ctx.target][1]
+
+    text = re.sub("\w\w\w\w+", imouto_factory('nida'), text)
     ctx.message(text)
